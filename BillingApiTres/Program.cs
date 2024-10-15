61b906fdee30c1b031d315a328d1c6c6c962fcbf
Sanghun.Han <Sanghun.Han@cloudmt.co.kr>
2024-10-15 15:25:44 +0900

    [#3124] 엔터티 모델 추가를 위한 프로젝트 추가 및 겸사겸사 모델도 추가

--- BillingApiTres/Program.cs
+++ BillingApiTres/Program.cs
@@ -19,11 +19,11 @@ namespace BillingApiTres
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
