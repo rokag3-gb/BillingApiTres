1e7204afeca3b1c37f4512cb384d35c839e06664
Sanghun.Han <Sanghun.Han@cloudmt.co.kr>
2024-10-22 18:46:59 +0900

    [#3123] DTO 작성 및 경로 변경

---
+++ BillingApiTres/Models/Dto/ServiceHierarchy/ServiceHierarchyAddRequest.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration.Annotations;
+using Billing.Data.Models;
+using BillingApiTres.Models.Validations;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace BillingApiTres.Models.Dto
+{
+    [AutoMap(typeof(ServiceHierarchy), ReverseMap = true)]
+    public record ServiceHierarchyAddRequest
+    {
+        [Required]
+        [SourceMember(nameof(ServiceHierarchy.TenantId))]
+        public string TenantId { get; set; }
+
+        [Required]
+        [SourceMember(nameof(ServiceHierarchy.ParentAccId))]
+        public long ContractorId { get; set; }
+
+        [Required]
+        [SourceMember(nameof(ServiceHierarchy.AccountId))]
+        public long ContracteeId { get; set; }
+
+        [Required]
+        [SourceMember(nameof(ServiceHierarchy.IsActive))]
+        public bool IsActive { get; set; }
+
+        [Required]
+        [Iso8601UtcValidation]
+        [SourceMember(nameof(ServiceHierarchy.StartDate))]
+        public DateTime ContractDate { get; set; }
+
+        [Required]
+        [Iso8601UtcValidation]
+        [SourceMember(nameof(ServiceHierarchy.EndDate))]
+        public DateTime ExpireDate { get; set; }
+    }
+}
---
+++ BillingApiTres/Models/Dto/ServiceHierarchy/ServiceHierarchyAddResponse.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration.Annotations;
+using Billing.Data.Models;
+using BillingApiTres.Models.Validations;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace BillingApiTres.Models.Dto
+{
+    [AutoMap(typeof(ServiceHierarchy))]
+    public record ServiceHierarchyAddResponse
+    {
+        [SourceMember(nameof(ServiceHierarchy.Sno))]
+        public long? SerialNo { get; set; } = null;
+
+        [Required]
+        [SourceMember(nameof(ServiceHierarchy.TenantId))]
+        public string TenantId { get; set; }
+
+        [Required]
+        [SourceMember(nameof(ServiceHierarchy.ParentAccId))]
+        public long ContractorId { get; set; }
+
+        [Required]
+        [SourceMember(nameof(ServiceHierarchy.AccountId))]
+        public long ContracteeId { get; set; }
+
+        [Required]
+        [SourceMember(nameof(ServiceHierarchy.IsActive))]
+        public bool IsActive { get; set; }
+
+        [Required]
+        [Iso8601UtcValidation]
+        [SourceMember(nameof(ServiceHierarchy.StartDate))]
+        public DateTime ContractDate { get; set; }
+
+        [Required]
+        [Iso8601UtcValidation]
+        [SourceMember(nameof(ServiceHierarchy.EndDate))]
+        public DateTime ExpireDate { get; set; }
+    }
+}
