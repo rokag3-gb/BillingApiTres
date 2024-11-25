a12f691a947ccb398e43583f2ecd4f93f62adc94
Sanghun.Han <Sanghun.Han@cloudmt.co.kr>
2024-11-25 10:19:37 +0900

    [#3216] 타임존 헤더 유무를 검사하는 미들웨어 추가

--- BillingApiTres/Program.cs
+++ BillingApiTres/Program.cs
@@ -125,6 +125,8 @@ namespace BillingApiTres
             app.UseHttpsRedirection();
             app.MapControllers();
 
+            app.UseTimezoneHeaderChecker();
+
             app.Run();
         }
     }
