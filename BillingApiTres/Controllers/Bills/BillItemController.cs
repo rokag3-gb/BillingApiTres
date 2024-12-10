920a8a79a58e3db4b7371bb1e17f0ad9492110cc
Sanghun.Han <Sanghun.han@cloudmt.co.kr>
2024-12-10 18:25:19 +0900

    [#3345] Bill Item 조회 컨트롤러 로직 작성

---
+++ BillingApiTres/Controllers/Bills/BillItemController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Billing.Data.Interfaces;
+using Billing.Data.Models.Bill;
+using BillingApiTres.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Immutable;
+
+namespace BillingApiTres.Controllers.Bills
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class BillItemController(IBillItemRepository billItemRepository,
+                                    IMapper mapper,
+                                    IConfiguration config) : ControllerBase
+    {
+        [HttpGet("/bills/{billId}/billItems")]
+        public ActionResult<PaginationResponse<BillItemResponse>> GetList(
+            long billId,
+            [FromQuery] BillItemRequest request)
+        {
+            var items = billItemRepository.GetList(billId, request.Offset, request.Limit);
+
+            if (items.Any() == false)
+                return NotFound(new { BillId = billId });
+
+            var accountId = items.FirstOrDefault()?.Bill?.SellerAccountId;
+            var availableAccountIds = HttpContext.Items[config["AccountHeader"]!] as ImmutableHashSet<long>;
+
+            if (availableAccountIds?.Contains(accountId ?? -1) == false)
+                return Forbid();
+
+            var response = items.Select(i => mapper.Map<BillItemResponse>(i)).ToList();
+
+            return new PaginationResponse<BillItemResponse>(response,
+                                                            items.Count,
+                                                            request.Offset,
+                                                            request.Limit);
+        }
+    }
+}
