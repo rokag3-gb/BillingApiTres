a1803d6025eb90810f96e53dd04c0b38001e5f1d
Sanghun.Han <Sanghun.han@cloudmt.co.kr>
2024-12-13 14:43:45 +0900

    [#3373] 사용량 조회 dto 작성

---
+++ BillingApiTres/Models/Dto/Usage/UsageRequest.cs
@@ -0,0 +1,40 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace BillingApiTres.Models.Dto
+{
+    public enum UsageUnit
+    {
+        [EnumMember(Value = "monthly")]
+        Monthly,
+        [EnumMember (Value = "daily")]
+        Daily
+    }
+
+    public record UsageRequest
+    {
+        /// <summary>
+        /// 사용량 단위. 월단위, 일단위...
+        /// </summary>
+        [Required]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public UsageUnit UsageUnit { get; set; }
+
+        /// <summary>
+        /// 사용량 조회 대상의 account id. csv 형식.
+        /// </summary>
+        [Required]
+        public string AccountIds { get; set; }
+
+        [Required]
+        public DateTime From { get; set; }
+
+        [Required]
+        public DateTime To { get; set; }
+
+        public int? Offset { get; set; }
+        public int? Limit { get; set; }
+    }
+}
