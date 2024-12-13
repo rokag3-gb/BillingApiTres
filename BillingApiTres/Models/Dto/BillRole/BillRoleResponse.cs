ce428a9ef0a2e695bf91d3f6f54061a3c8ef194c
Sanghun.Han <Sanghun.han@cloudmt.co.kr>
2024-12-13 17:08:42 +0900

    [#3381] billing role 조회 dto, 컨트롤러 작성

---
+++ BillingApiTres/Models/Dto/BillRole/BillRoleResponse.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration.Annotations;
+using Billing.Data.Models.Bill;
+
+namespace BillingApiTres.Models.Dto
+{
+    [AutoMap(typeof(BillingRole))]
+    public record BillRoleResponse
+    {
+        [SourceMember(nameof(BillingRole.RoleId))]
+        public string BillRoleId { get; set; }
+        [SourceMember(nameof(BillingRole.Alias))]
+        public string RoleName { get; set; }
+    }
+}
