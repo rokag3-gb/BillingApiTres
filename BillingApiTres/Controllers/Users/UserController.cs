c38e1c4552306a58d7c271be67ae9abb22000d1c
Sanghun.Han <Sanghun.han@cloudmt.co.kr>
2024-12-17 16:14:39 +0900

    [#3382] 사용자 초대 및 패스워드 변경 api 추가

---
+++ BillingApiTres/Controllers/Users/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Core;
+using BillingApiTres.Converters;
+using BillingApiTres.Models.Clients;
+using BillingApiTres.Models.Dto.Users;
+using BillingApiTres.Models.Validations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using BillingApiTres.Extensions;
+
+namespace BillingApiTres.Controllers.Users
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class UserController(AcmeGwClient gwClient, ILogger<UserController> logger) : ControllerBase
+    {
+        [HttpPost("/user-invite")]
+        [AuthorizeAccountIdFilter([nameof(UserInviteRequest.AccountId)])]
+        public async Task<ActionResult> Invite([FromBody] UserInviteRequest request)
+        {
+            var response = await gwClient.GetSerializedString("iam/user-invite",
+                                                              HttpMethod.Post,
+                                                              JsonContent.Create(request));
+            return Ok();
+        }
+
+        [HttpPost("/user/{userId}/forgot-password")]
+        public async Task<ActionResult> ChangePassword(string userId)
+        {
+            var token = JwtConverter.ExtractJwtToken(HttpContext.Request);
+            var accountUsers = await gwClient.Get<List<AccountUser>>($"sales/accountUser?limit=99999&userUuid={userId}", token?.RawData!);
+
+            if (HttpContext.AuthenticateAccountId(accountUsers.Select(u => u.AccountId)) == false)
+                return Forbid();
+
+            var response = await gwClient.GetSerializedString($"iam/user/{userId}/forgot-password",
+                                                              HttpMethod.Post);
+            return Ok();
+        }
+    }
+}
+
+
---
+++ BillingApiTres/Models/Dto/Users/UserChangePasswordRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BillingApiTres.Models.Dto.Users
+{
+    public record UserChangePasswordRequest
+    {
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string PasswordConfirm { get; set; }
+        [Required]
+        public string Token { get; set; }
+    }
+}
