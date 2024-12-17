225f9937d2c38f6972f7a84fe0058f4b2d71c8b5
Sanghun.Han <Sanghun.han@cloudmt.co.kr>
2024-12-17 16:13:30 +0900

    [#3382] 사용자 목록 조회 및 삭제 api 추가

---
+++ BillingApiTres/Controllers/AccountUsers/AccountUserController.cs
@@ -0,0 +1,43 @@
+﻿using BillingApiTres.Converters;
+using BillingApiTres.Extensions;
+using BillingApiTres.Models.Clients;
+using BillingApiTres.Models.Validations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BillingApiTres.Controllers.AccountUsers
+{
+
+    [Route("[controller]")]
+    [Authorize]
+    public class AccountUserController(AcmeGwClient gwClient,
+                                       IConfiguration config) : ControllerBase
+    {
+        [AuthorizeAccountIdFilter([nameof(accountId)])]
+        [HttpGet("/accountUsers")]
+        public async Task<string> GetList([FromQuery][Required] long accountId)
+        {
+            var response = await gwClient.GetSerializedString(
+                $"sales/accountUser?limit=999999&accountIdCsv={accountId}",
+                HttpMethod.Get);
+            return response;
+        }
+
+        [HttpDelete("/accountUsers/{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var token = JwtConverter.ExtractJwtToken(HttpContext.Request);
+
+            var accountUser = await gwClient.Get<AccountUser>($"sales/accountUser/{id}", token?.RawData!);
+            if (accountUser == null)
+                return NoContent();
+
+            if (HttpContext.AuthenticateAccountId([accountUser.AccountId]) == false)
+                return Forbid();
+
+            await gwClient.GetSerializedString($"sales/accountUser/{id}", HttpMethod.Delete);
+            return NoContent();
+        }
+    }
+}
