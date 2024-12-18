dd1d8faad1fc796199c7a120e722d0f3f5136158
Sanghun.Han <Sanghun.Han@cloudmt.co.kr>
2024-12-18 16:39:09 +0900

    [#3386] product 조회 api controller 작성

---
+++ BillingApiTres/Models/Dto/Products/ProductListResponse.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Billing.Data.Models.Bill;
+
+namespace BillingApiTres.Models.Dto.Products
+{
+    [AutoMap(typeof(Product))]
+    public record ProductListResponse
+    {
+        public long ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public string Remark { get; set; } = string.Empty;
+        public DateTime SavedAt { get; set; }
+        public string SaverId { get; set; } = string.Empty;
+    }
+}
