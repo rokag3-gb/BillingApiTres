fbb3799eff25431235c5b19359870be87ad12673
Sanghun.Han <Sanghun.han@cloudmt.co.kr>
2024-12-23 15:25:14 +0900

    [#3387] 청구서 상태 갱신 repository 로직 작성

--- Billing.Data/Interfaces/IBillRepository.cs
+++ Billing.Data/Interfaces/IBillRepository.cs
@@ -4,7 +4,8 @@ namespace Billing.Data.Interfaces
 {
     public interface IBillRepository
     {
-        List<Bill> GetRange(DateTime from, DateTime to, List<long> accountIds, int? offset, int? limit);
+        List<Bill> GetRange(DateTime? from, DateTime? to, List<long>? accountIds, List<long>? billIds, int? offset, int? limit);
         List<Bill> GetRangeWithRelations(DateTime from, DateTime to, List<long> accountIds, int? offset, int? limit);
+        int UpdateStatus(string statusCode, IEnumerable<long> billIds);
     }
 }
--- Billing.EF/Repositories/BillRepository.cs
+++ Billing.EF/Repositories/BillRepository.cs
@@ -7,12 +7,19 @@ namespace Billing.EF.Repositories
 {
     public class BillRepository(BillContext billContext, ILogger<BillRepository> logger) : IBillRepository
     {
-        public List<Bill> GetRange(DateTime from, DateTime to, List<long> accountIds, int? offset, int? limit)
+        public List<Bill> GetRange(DateTime? from, DateTime? to, List<long>? accountIds, List<long>? billIds, int? offset, int? limit)
         {
             List<Bill> bills = new();
-            var query = billContext.Bills
-                .Where(b => accountIds.Contains(b.BuyerAccountId) || accountIds.Contains(b.SellerAccountId))
-                .Where(b => b.BillDate >= from && b.BillDate <= to);
+            var query = billContext.Bills.AsQueryable();
+
+            if (from.HasValue && to.HasValue)
+                query = query.Where(b => b.BillDate >= from.Value && b.BillDate <= to.Value);
+
+            if (accountIds?.Any() == true)
+                query = query.Where(b => accountIds.Contains(b.BuyerAccountId) || accountIds.Contains(b.SellerAccountId));
+
+            if (billIds?.Any() == true)
+                query = query.Where(b => billIds.Contains(b.BillId));
 
             if (offset != null && limit != null)
                 bills = query.Skip(offset.Value).Take(limit.Value).ToList();
@@ -34,8 +41,17 @@ namespace Billing.EF.Repositories
 
             if (offset != null && limit != null)
                 query = query.Skip(offset.Value).Take(limit.Value);
-            
+
             return query.ToList();
+        }
+
+        public int UpdateStatus(string statusCode, IEnumerable<long> billIds)
+        {
+            var count = billContext.Bills
+                .Where(b => billIds.Contains(b.BillId))
+                .ExecuteUpdate(b => b.SetProperty(p => p.StatusCode, statusCode));
+
+            return count;
         }
     }
 }
