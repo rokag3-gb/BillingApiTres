d956584744f1c2020dec097e2e3644137244d326
Sanghun.Han <Sanghun.Han@cloudmt.co.kr>
2024-12-29 23:55:00 +0900

    automapper 프로필 수정

---
+++ BillingApiTres/Models/MapperProfiles/BillProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Billing.Data.Models.Bill;
+using BillingApiTres.Extensions;
+using BillingApiTres.Models.Dto;
+
+namespace BillingApiTres.Models.MapperProfiles
+{
+    public class BillProfile : Profile
+    {
+        public BillProfile() 
+        {
+            CreateMap<NcpDetail, BillDetailResponse>();
+            CreateMap<Bill, Bill>().IgnoreKeyProperties();
+            CreateMap<BillItem, BillItem>().IgnoreKeyProperties();
+            CreateMap<BillDetail, BillDetail>().IgnoreKeyProperties();
+            CreateMap<Product, Product>().IgnoreKeyProperties();
+        }
+    }
+}
