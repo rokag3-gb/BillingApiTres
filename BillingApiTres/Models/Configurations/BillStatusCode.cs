48e00f9e6830ed86a4b44950693c7b44b58c0cde
Sanghun.Han <Sanghun.Han@cloudmt.co.kr>
2025-01-13 17:03:32 +0900

    [#3432] 비즈니스 로직에서 사용하는 코드를 구성 요소로 추가함

---
+++ BillingApiTres/Models/Configurations/BillStatusCode.cs
@@ -0,0 +1,8 @@
+﻿namespace BillingApiTres.Models.Configurations
+{
+    public record BillStatusCode
+    {
+        public string Certain { get; set; } = "BST-EXT";
+        public string Uncertain { get; set; } = "BST-001";
+    }
+}
---
+++ BillingApiTres/Models/Configurations/ContractChargeCode.cs
@@ -0,0 +1,9 @@
+﻿namespace BillingApiTres.Models.Configurations
+{
+    public record ContractChargeCode
+    {
+        public string NcpPcpDiscount { get; set; } = "PcpContractDiscountRate";
+        public string NcpWorkDiscount { get; set; } = "WorksContractDiscountRate";
+        public string MspCharge { get; set; } = "MspChargeRate";
+    }
+}
